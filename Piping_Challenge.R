a67003c38f630b2179f3789b5fec0768742ad65a
gndaskalova <gndaskalova@gmail.com>
2017-02-24 08:22:06 +0000

    Add files via upload

---
+++ Piping_Challenge.R
@@ -0,0 +1,14 @@
+# TeamShrub piping challenge
+# Calculate the LPI for each country within the LPI database
+# Data available from http://www.livingplanetindex.org/home/index
+
+# 24-02-2016
+
+# Libraries ----
+
+# Load data ----
+
+# Calculate country-specific LPI ----
+
+# Visualise LPI for each country and save the plots ----
+# Loops or pipes? Can you save individual files for each plot and create panels with e.g. 6 countries per panel?
